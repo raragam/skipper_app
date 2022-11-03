9dd3e95ae977f8ff9046ad6a2be868e75988c562
raragam <104993317+raragam@users.noreply.github.com>
2022-11-03 11:47:19 +0000

    red mozi

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import 'jquery'
+import "script.js"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
